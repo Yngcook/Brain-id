c37aabc097ba66bc383f9806f3f3ef15f47c8848
ThugKuk <140739704+ThugKuk@users.noreply.github.com>
2023-10-02 11:27:29 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -11,32 +11,26 @@ import pytorch_lightning as pl
 from pytorch_lightning.loggers import CSVLogger
 import torch.nn.functional as F
 import json
+from torchmetrics import Accuracy
+#import pandas as pd
+#import seaborn as sn
+# import matplotlib.pyplot as plt
 
-def positional_encoding_1d(d_model, length):
-    if d_model % 2 != 0:
-        raise ValueError("Cannot use sin/cos positional encoding with "
-                         "odd dim (got dim={:d})".format(d_model))
-    pe = torch.zeros(length, d_model)
-    position = torch.arange(0, length).unsqueeze(1)
-    div_term = torch.exp((torch.arange(0, d_model, 2, dtype=torch.float) *
-                          -(math.log(10000.0) / d_model)))
-    pe[:, 0::2] = torch.sin(position.float() * div_term)
-    pe[:, 1::2] = torch.cos(position.float() * div_term)
-    return pe
 
 class EEGDataset(Dataset):
-    def __init__(self, data_dir, json_file, sample_length=5, transformer_model=None):
+    def __init__(self, data_dir, json_file, transformer_model=None):
         self.data_dir = data_dir
-        self.sample_length = sample_length
-        self.transformer_model = transformer_model
 
         with open(json_file, 'r') as json_file:
             self.data_dict = json.load(json_file)
+            #создаем тренировочную выборку
+            keys = list(self.data_dict.keys())[0:90]
+            self.data_train_dict = {key: self.data_dict[key] for key in keys}
 
         self.file_list = []
 
-        # Создаем список файлов для каждого человека
-        for person_folder, person_files in self.data_dict.items():
+        #Создаем список файлов для каждого человека
+        for person_folder, person_files in self.data_train_dict.items():
             person_file_paths = [os.path.join(data_dir, person_folder, file_name) for file_name in person_files]
             self.file_list.extend([(person_folder, file_path) for file_path in person_file_paths])
 
@@ -48,116 +42,95 @@ class EEGDataset(Dataset):
         raw = mne.io.read_raw_edf(file_path, preload=True, verbose=False)
         data = raw.get_data()
 
-        # Находим количество точек в 5-ти секундах (умножаем частоту дискретизации на длину семпла)
-        sample_length_in_points = int(self.sample_length * raw.info['sfreq'])
+        #slidding window
+        seconds_per_minute = 60
+        samples_per_minute = seconds_per_minute * raw.info['sfreq']
+        max_index = data.shape[1] - samples_per_minute
+        start_index = random.randint(0, max_index)
+        minute_data = data[:, start_index:int(start_index) + int(samples_per_minute)]
+        window_size_seconds = 1
+        window_size_samples = int(window_size_seconds*raw.info['sfreq'])
+        window = np.lib.stride_tricks.sliding_window_view(minute_data, (64, window_size_samples))[0, ::4]
 
-        # Создаем список для хранения двух кусков данных
-        data_list = []
-
-        for _ in range(2):
-            start_time = random.randint(0, data.shape[1] - sample_length_in_points)
-
-            # Срезаем 5 секунд
-            data_chunk = data[:, start_time:start_time + sample_length_in_points]
-            data_list.append(data_chunk)
-
-        data_tensor = np.stack(data_list).astype(np.float32)
+        #получаем ID человека, которому пренадлежит EEG сигнал
         person_tensor = int(person_folder[1:])
 
         return {
-            'data': data_tensor,
+            'data': window,
             'person_id': person_tensor,
         }
 
 class EEGDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir, json_file, batch_size, sample_length=5):
+    def __init__(self, data_dir, json_file, batch_size):
         super().__init__()
         self.data_dir = data_dir
         self.json_file = json_file
         self.batch_size = batch_size
-        self.sample_length = sample_length
 
-    def setup(self, stage=None):
-        self.dataset = EEGDataset(self.data_dir, self.json_file, self.sample_length)
-        self.train_dataset, self.val_dataset = train_test_split(self.dataset, test_size=0.2, random_state=42)
+    # def setup(self, stage=None):
+    #   if stage == 'fit' or stage is None:
+    #     self.dataset = EEGDataset(self.data_dir, self.json_file)
+
+    #   if stage == 'test' or stage is None:
+    #     self.dataset_test = [EEGDataset(self.data_dir, self.json_file) for _ in range(3)]
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=8)
+        return DataLoader(EEGDataset(self.data_dir, self.json_file), batch_size=self.batch_size, shuffle=True, num_workers=1)
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=True, num_workers=8)
+        return DataLoader(EEGDataset(self.data_dir, self.json_file), batch_size=self.batch_size, shuffle=False, num_workers=1)
 
-class TransformerModel(pl.LightningModule):
-    def __init__(self, d_model=256, nhead=8, dim_feedforward=1024, num_layers=6,
-                 input_dim=64, learning_rate=2e-4, n_channels=64):
-        super(TransformerModel, self).__init__()
+    def test_dataloader(self):
+        return DataLoader(EEGDataset(self.data_dir, self.json_file), batch_size=self.batch_size, shuffle=False, num_workers=1)
+
+class CNNModel(pl.LightningModule):
+    def __init__(self, d_model=256, input_channels=64, learning_rate=2e-4, dropout_prob=0.5, num_classes=90):
+        super(CNNModel, self).__init__()
+        self.relu = nn.ReLU(inplace=True)
         self.d_model = d_model
         self.learning_rate = learning_rate
-        self.transformer = nn.TransformerEncoder(nn.TransformerEncoderLayer(
-                d_model=d_model, nhead=nhead, dim_feedforward=dim_feedforward,
-                dropout=0.0, batch_first=True),
-            num_layers=num_layers)
-        self.input_proj = nn.Linear(input_dim, d_model)
-        self.cls_token = nn.Embedding(1, d_model)
 
+        self.conv1 = nn.Conv2d(input_channels, 128, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = nn.Linear(512, d_model)
+        self.dropout = nn.Dropout(p=0)
+        self.fc2 = nn.Linear(d_model, num_classes)
+
+        self.val_accuracy = Accuracy(task="multiclass", num_classes=90)
 
     def forward(self, data):
+        #CNN layers
         data = data * 10**4
-        batch_size, n_timestamps, n_channels = data.size()
-        cls_token = self.cls_token.weight.unsqueeze(0)
-        cls_token = cls_token.expand(batch_size, 1, -1)
-        data = self.input_proj(data)
-        pe = positional_encoding_1d(self.d_model, n_timestamps).unsqueeze(0).expand(batch_size, -1, -1)
-        data = data + pe
-        data = torch.cat((data, cls_token), dim=1)
-        vector = self.transformer(data)[:, -1]
+        data = data.permute(0, 2, 3, 1)
+        data = self.relu(self.conv1(data))
+        data = self.maxpool(data)
+        data = self.relu(self.conv2(data))
+        data = self.maxpool(data)
+        data = self.relu(self.conv3(data))
+
+        data = F.avg_pool2d(data, kernel_size=data.size()[2:])
+
+        data = data.view(data.size(0), -1)
+
+        data = F.relu(self.fc1(data))
+
+        data = self.dropout(data)
+
+        vector = self.fc2(data)
 
         return vector
 
-    def cosine_similarity(self, vector):
-        cosine_sim_matrix = F.cosine_similarity(vector.unsqueeze(1), vector.unsqueeze(0), dim=-1)
-        return cosine_sim_matrix
-
-    def triplet_loss(self, cosine_similarity_matrix, tensor_person, margin=0.5):
-        assert cosine_similarity_matrix.shape[0] == cosine_similarity_matrix.shape[1]
-        assert tensor_person.shape[0] == tensor_person.shape[1]
-
-        tensor_person = tensor_person.to(torch.bool)
-
-        n = cosine_similarity_matrix.shape[0]
-        inf = 2.0
-
-        # Min positive
-        pos_gt = torch.logical_and(
-            tensor_person,
-            torch.logical_not(torch.eye(n, dtype=torch.bool)))
-        min_pos = torch.where(pos_gt, cosine_similarity_matrix, inf)
-        min_pos = torch.min(min_pos, dim=1).values
-
-        # Max negative
-        max_neg = torch.where(tensor_person, -inf, cosine_similarity_matrix)
-        max_neg = torch.max(max_neg, dim=1).values
-
-        loss = torch.clamp(max_neg - min_pos + margin, 0.0).mean()
-
-        return loss
-
     def training_step(self, batch, batch_idx):
-        data = batch['data']
+        data = batch['data'].float()
         tensor_person = batch['person_id']
-        tensor_person = tensor_person.view(-1, 1)
-        tensor_person = torch.repeat_interleave(tensor_person, 2, dim=0)
-        tensor_person = tensor_person / tensor_person.T
-        tensor_person[tensor_person != 1] = 0
 
-        data = data.view(-1, data.size(-2), data.size(-1)).permute(0, 2, 1)
-        vector = self(data)
+        loss = F.cross_entropy(self(data), tensor_person - 1)
 
-        cosine_similarity_matrix = self.cosine_similarity(vector)
-
-        loss = self.triplet_loss(cosine_similarity_matrix, tensor_person)
         self.log("train_loss", loss, on_epoch=True, on_step=True)
-
 
         if batch_idx == 0:
             print(f'Epoch {self.current_epoch}: Loss = {loss:.4f}')
@@ -165,112 +138,59 @@ class TransformerModel(pl.LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
-        data = batch['data']
-        person = batch['person_id']
-        person = [int(a) for a in person]
-        tensor_person = torch.tensor(person)
-        tensor_person = tensor_person.view(-1, 1)
-        tensor_person = torch.repeat_interleave(tensor_person, 2, dim=0)
-        tensor_person = tensor_person / tensor_person.T
-        tensor_person[tensor_person != 1] = 0
-        tensor_person = tensor_person.to(torch.bool)
-        inf = 2.0
+        data = batch['data'].float()
+        tensor_person = batch['person_id']
 
-        data = data.view(-1, data.size(-2), data.size(-1)).permute(0, 2, 1)
-        vector = self(data)
+        # #для каждого батча
+        # _, predicted = torch.argmax(outputs, 1)
+        # correct = (predicted == tensor_person-1).sum().item()
+        # accuracy = correct / len(predicted)
 
-        cosine_similarity_matrix = self.cosine_similarity(vector)
-        n = cosine_similarity_matrix.shape[0]
+        loss = F.cross_entropy(self(data), tensor_person - 1)
 
-        eye_tensor = torch.eye(n, dtype=torch.bool, device=data.device)
-        pos_gt = torch.logical_and(tensor_person, torch.logical_not(eye_tensor))
+        preds = torch.argmax(self(data), dim=1)
+        self.val_accuracy.update(preds, tensor_person - 1)
 
-
-        max_neg = torch.where(tensor_person, -inf, cosine_similarity_matrix)
-        max_neg = torch.max(max_neg, dim=1).values
-        min_pos = torch.where(pos_gt, cosine_similarity_matrix, inf)
-        min_pos = torch.min(min_pos, dim=1).values
-
-        # считаем процент строк для которых самый худший положительный лучше самого лучшего отрицательного
-        row_comparison = min_pos > max_neg
-
-        num_rows_min_pos_gt_max_neg = row_comparison.sum().item()
-
-        total_rows = row_comparison.size(0)
-
-        bad_accuracy = (num_rows_min_pos_gt_max_neg / total_rows) * 100.0
-
-        loss = self.triplet_loss(cosine_similarity_matrix, tensor_person)
-
-        self.log("val_loss", loss, on_epoch=True, on_step=False)
-        self.log("val_accuracy", bad_accuracy, on_epoch=True, on_step=True)
+        self.log("val_acc", self.val_accuracy, prog_bar=True)
+        self.log("val_loss", loss, prog_bar=True)
 
         if batch_idx == 0:
-            print(f"Epoch {self.current_epoch}: Validation Accuracy = {bad_accuracy:.4f}")
+            accuracy_value = self.val_accuracy.compute()
+            print(f"Epoch {self.current_epoch}: Validation Accuracy = {accuracy_value}")
 
-        return bad_accuracy
+        return {'loss': loss, 'accuracy': self.val_accuracy}
+
+    def test_step(self, batch, batch_idx):
+        return self.validation_step(batch, batch_idx)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
 
-# class Callback(pl.Callback):
-#     def on_epoch_start(self, trainer, pl_module):
-#         print(f"Epoch {trainer.current_epoch + 1}")
-#
-#     def on_epoch_end(self, trainer, pl_module):
-#         current_epoch = trainer.current_epoch
-#         loss_value = trainer.callback_metrics['loss']
-#         print(f'Epoch {current_epoch}: Loss = {loss_value:.4f}')
+#training
+data_dir = "/content/gdrive/MyDrive/files"
+json_file = "/content/gdrive/MyDrive/files/data_dict.json"
+dataset = EEGDataset(data_dir, json_file)
+batch_size = 8
 
+data_module = EEGDataModule(data_dir, json_file, batch_size)
 
-# путь к 109 файлам EEG сигналов и json файлу
-# data_dir = "./data/files"
-# json_file = "C:/Users/kukur/PycharmProjects/ProjectEEG/data/files/data_dict.json"
-# dataset = EEGDataset(data_dir, json_file)
-# # model = TransformerModel(learning_rate=config.learning_rate)
-# # data_module = EEGDataModule(data_dir="./data/files", batch_size=64, sample_length=5, transformer_model=model)
-# batch_size = 8
-# data_module = EEGDataModule(data_dir, json_file, batch_size, sample_length=5)
-# margin = 0.5
-# eeg_dataset = EEGDataset(data_dir=data_dir, sample_length=5, json_file=json_file)
-# dataloader = DataLoader(eeg_dataset, batch_size=batch_size, shuffle=True)
-# transformer_model = TransformerModel()
+#model = CNNModel()
 
-# for batch in dataloader:
-#     data = batch['data']  # Тензор с данными размером [8, 2, 64, 800]
-#     #создаем фактическую матрицу
-#     person = batch['person_id']
-#     person = [int(a) for a in person]
-#     tensor_person = torch.tensor(person)
-#     tensor_person = tensor_person.view(-1, 1)
-#     tensor_person = torch.repeat_interleave(tensor_person, 2, dim=0)
-#     tensor_person = tensor_person / tensor_person.T
-#     tensor_person[tensor_person != 1] = 0
-#
-#     data = data.view(-1, data.size(-2), data.size(-1)).permute(0, 2, 1)
-#     vector = transformer_model(data)
-#
-#     cosine_similarity_matrix = transformer_model.cosine_similarity(vector)
-#
-#     #создаем trepliet loss
-#     triplet_loss = transformer_model.loss(cosine_similarity_matrix, tensor_person, margin)
-#     break
+trainer = pl.Trainer(
+accelerator='gpu',
+max_epochs=15,
+logger=pl.loggers.CSVLogger("/content/gdrive/MyDrive/logs/"),
+)
 
-if __name__ == "__main__":
-    data_dir = "./data/files"
-    json_file = "C:/Users/kukur/PycharmProjects/ProjectEEG/data/files/data_dict.json"
-    dataset = EEGDataset(data_dir, json_file)
-    batch_size = 8
+#trainer.fit(model, data_module)
 
-    data_module = EEGDataModule(data_dir, json_file, batch_size, sample_length=5)
+model = CNNModel.load_from_checkpoint("/content/gdrive/MyDrive/logs/lightning_logs/version_7/checkpoints/epoch=14-step=2340.ckpt")
+model.eval()
+trainer.test(model, data_module)
 
-    transformer_model = TransformerModel()
-
-    trainer = pl.Trainer(
-        accelerator='cpu',
-        max_epochs=10,
-        logger=pl.loggers.CSVLogger("logs/"),
-    )
-
-    trainer.fit(transformer_model, data_module)
+# metrics = pd.read_csv(f"{trainer.logger.log_dir}/metrics.csv")
+# del metrics["step"]
+# metrics.set_index("epoch", inplace=True)
+# display(metrics.dropna(axis=1, how="all").head())
+# sn.relplot(data=metrics, kind="line")
